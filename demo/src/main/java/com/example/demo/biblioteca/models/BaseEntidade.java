f09920d3276d9a9867a32486b4c9ba24c699f00b
Filipe <filipess671@gmail.com>
2023-06-22 17:09:34 -0300

    Chore: Novo repositorio base

---
+++ demo/src/main/java/com/example/demo/biblioteca/models/BaseEntidade.java
@@ -0,0 +1,22 @@
+package com.example.demo.biblioteca.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ demo/src/main/java/com/example/demo/biblioteca/repositories/BaseRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.example.demo.biblioteca.repositories;
+
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+
+import java.io.Serializable;
+import jakarta.persistence.EntityManager;
+
+public class BaseRepositoryImpl<T, ID extends Serializable>
+        extends SimpleJpaRepository<T, ID> implements BaseRepository<T, ID> {
+
+    private final EntityManager entityManager;
+
+    public BaseRepositoryImpl(JpaEntityInformation entityInformation,
+            EntityManager entityManager) {
+        super(entityInformation, entityManager);
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void refresh(T t) {
+        entityManager.refresh(t);
+    }
+}
